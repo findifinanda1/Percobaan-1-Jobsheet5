6c8c924fd3c4c2153548ddffa02325459486e70a
findifinanda1 <findifinandaaszahra@gmail.com>
2025-10-04 17:00:02 +0700

    Modifikasi Percobaan 1

--- ifCetakKRS13.java
+++ ifCetakKRS13.java
@@ -8,6 +8,9 @@ public class ifCetakKRS13 {
     if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    }else  {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
     }
     }
-}
+
